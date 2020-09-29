067d5b6e96416e5e17e0c547fefa3fda8d0bf00b
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2020-09-29 13:37:23 +0600

    IO task added for convenience, original with commit history is in 2nd round folder

---
+++ JavaTasks/src/main/java/HomeTasksForEpam/io/maintask/Main.java
@@ -0,0 +1,17 @@
+package io.maintask;;
+import java.io.File;
+import java.io.IOException;
+
+
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+//         File file = new File("C:\\Users\\PC\\Desktop\\Torrent");
+        // File file = new File("C:\\Users\\PC\\Desktop\\Torrent\\outputFileWithSticks.txt");
+        // File file = new File("C:\\Users\\PC\\Desktop\\Misc");
+        File file = new File("C:\\Users\\PC\\Desktop\\Torrent\\outputFileWithSticks.txt");
+        FileInput fileInput = new FileInput();
+        fileInput.readFile(file.getPath());
+    }
+}
+
---
+++ JavaTasks/src/main/java/HomeTasksForEpam/io/maintask/OutputTextFilePostProcessor.java
@@ -0,0 +1,59 @@
+package io.maintask;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class OutputTextFilePostProcessor {
+    public static void addSomeSticks(File file) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file.getPath() + File.separator + "filelist.txt"));
+        ArrayList<String> listOfLines = new ArrayList<>();
+        File outputFileWithSticks = new File(file.getPath() + File.separator + "outputFileWithSticks.txt");
+        OutputStream outputStream = new FileOutputStream(outputFileWithSticks);
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
+        BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+        bufferedWriter.write(file.getName() + System.lineSeparator());
+
+        String line = bufferedReader.readLine();
+        while (line != null) {
+            listOfLines.add(line);
+            line = bufferedReader.readLine();
+        }
+        for (int i = 0; i < listOfLines.size(); i++) {
+            int lastPlace = 0;
+            if (listOfLines.get(i).contains("└───")) {
+                for (int j = i - 1; j > lastPlace; j--) {
+                    if (listOfLines.get(j).length() > listOfLines.get(i).indexOf("└")) {
+                        char charAtIndex = listOfLines.get(j).charAt(listOfLines.get(i).indexOf("└"));
+                        if (charAtIndex == ' ') {
+                            String temp = replaceChar(listOfLines.get(j), '|', listOfLines.get(i).indexOf("└"));
+                            listOfLines.set(j, temp);
+                        } else if (charAtIndex == '└') {
+                            String temp = replaceChar(listOfLines.get(j), '├', listOfLines.get(i).indexOf("└"));
+                            listOfLines.set(j, temp);
+                        } else if (charAtIndex != ' ') {
+                            break;
+                        }
+                    }
+                }
+                lastPlace = i;
+            }
+            bufferedReader.close();
+        }
+        for (int i = 0; i < listOfLines.size(); i++) {
+            appendToTextFile(outputFileWithSticks, listOfLines.get(i) + System.lineSeparator());
+        }
+    }
+
+    public static String replaceChar(String str, char ch, int index) {
+        StringBuilder myString = new StringBuilder(str);
+        myString.setCharAt(index, ch);
+        return myString.toString();
+    }
+
+    public static void appendToTextFile(File filelist, String stringToAppend) throws IOException {
+        Writer output;
+        output = new BufferedWriter(new FileWriter(filelist, true));
+        output.append(stringToAppend);
+        output.close();
+    }
+}
